7e133aa02f1feef055a433aa9604bbbb9f9954f7
Anatom <49984698+anatom3000@users.noreply.github.com>
2022-05-01 09:16:01 +0200

    Revert "still doesnt work"
    This reverts commit 02d1915d31e26ab2694b2d939f3128dca409d912.

--- main.py
+++ main.py
@@ -1,68 +1,57 @@
 import numpy as np
 
-np.seterr(all='raise')  # for debugging
-
-import pygame
-from pygame.locals import *
-
-from sys import exit
+from ursina import *
 
 import physics
 
-<<<<<<< HEAD
-RESOLUTION = (640, 480)
-
-=======
 window.title = "Yet another Gravit"  # The window title
 window.borderless = False  # Show a border
 window.fullscreen = False  # Do not go Fullscreen
 window.aspect_ratio = 4 / 3
+window.vsync = False
 
-camera.orthographic = True
+camera.orthographic = False
 
 # window.exit_button.visible = False      # Do not show the in-game red X that loses the window
 # window.fps_counter.enabled = True       # Show the FPS (Frames per second) counter
 
 app = Ursina()
 
->>>>>>> parent of 75e08ca (refactored gravity but it doesn't work)
-base_mass = 1
+base_mass = 1e-12
 universe = physics.Universe(
     np.array(
         [
-            physics.Particle(mass=base_mass, position=np.array([2.0, 2.0])),
+            physics.Particle(mass=base_mass, position=np.array([+2.0, +2.0])),
             #    physics.Particle(mass=base_mass, position=np.array([+2.0, -2.0])),
             #    physics.Particle(mass=base_mass, position=np.array([-2.0, +2.0])),
-            physics.Particle(mass=base_mass, position=np.array([0.0, 0.0])),
+            physics.Particle(mass=base_mass, position=np.array([-2.0, -2.0])),
         ]
     ),
-    constant=100,
+    constant=2,
 )
 
-pygame.init()
+particles = np.array(
+    [
+        Entity(model="sphere", color=color.orange, scale=2, position=p.position)
+        for p in universe.particles
+    ]
+)
 
-screen = pygame.display.set_mode(RESOLUTION)
-clock = pygame.time.Clock()
+started = False
+ZOOM_MULTIPLIER = 1.1
 
-running = True
-while running:
-    screen.fill((0,) * 3)
-    for ev in pygame.event.get():
-        if ev.type == QUIT:
-            running = False
 
-    pygame.display.flip()
+def input(key):
+    global started
 
-    for particle in universe:
-        particle.display(screen)
+    if key == "space":
+        started = not started
 
-    dt = clock.tick() / 1000
-    universe.tick(dt)
+    if key == "scroll up":
+        camera.fov = max(5.0, camera.fov / ZOOM_MULTIPLIER)
+    if key == "scroll down":
+        camera.fov = min(150.0, camera.fov * ZOOM_MULTIPLIER)
 
-<<<<<<< HEAD
-pygame.quit()
-exit(0)
-=======
 
 def update():
     if started:
@@ -71,11 +60,11 @@ def update():
     # all_positions = np.empty((2, 2))
     for particle, entity_particle in zip(universe.particles, particles):
         entity_particle.position = particle.position
-        print(particle)
+        if started:
+            print(particle)
 
     if mouse.left:
         camera.position -= mouse.velocity / (time.dt * 2) * camera.fov / 90
 
 
 app.run()
->>>>>>> parent of 75e08ca (refactored gravity but it doesn't work)
--- physics.py
+++ physics.py
@@ -1,77 +1,55 @@
 import numpy as np
 
-<<<<<<< HEAD
-from particle import Particle
+N_DIM = 2
 
-=======
->>>>>>> parent of 75e08ca (refactored gravity but it doesn't work)
 
-def unit(a: np.array):
+def unit(a):
     return a / np.linalg.norm(a)
 
 
-<<<<<<< HEAD
-=======
 class Particle:
     def __init__(
             self,
             mass: float,
-            position: np.array = np.array([0.0, 0.0]),
-            velocity: np.array = np.array([0.0, 0.0]),
-            acceleration: np.array = np.array([0.0, 0.0])
+            position: np.array = np.zeros(N_DIM),
+            velocity: np.array = np.zeros(N_DIM),
     ):
         self.mass = mass
         self.position = position
         self.velocity = velocity
-        self.acceleration = acceleration
-        self.forces = np.array([])
+        self.acceleration = np.zeros(N_DIM)
+        self.resulting_force = np.zeros(N_DIM)
 
     def __str__(self):
-        return f"Particle(mass={self.mass}, position={self.position}, velocity={self.velocity}), acceleration={self.acceleration}"
+        return f"Particle(mass={self.mass}, position={self.position}, velocity={self.velocity}), acceleration={self.acceleration}, force={self.resulting_force}"
 
-class
+    def compute_state(self, dtime: float):
+        self.acceleration = self.resulting_force / self.mass
+        self.velocity = self.acceleration * dtime
+        self.position += self.velocity * dtime
 
->>>>>>> parent of 75e08ca (refactored gravity but it doesn't work)
+
 class Universe:
     def __init__(self, particles: np.array, constant: float = 1):
         self.particles = particles
         self.constant = constant
 
-<<<<<<< HEAD
     @staticmethod
     def tick_two_by_two(particle1: Particle, particle2: Particle):
         distance = particle2.position - particle1.position
         force = particle1.mass * particle2.mass / (
-                np.sum(np.square(distance))
+                np.linalg.norm(distance) ** 2
         )
 
         a2b = unit(distance)
+        print(a2b)
         return force * a2b
 
     def tick(self, dtime: float):
         for particle in self.particles:
-            particle.force = np.zeros(2)
-=======
-    def tick(self, dtime: float):
-        for particle in self.particles:
-            particle.acceleration = np.zeros(2)
->>>>>>> parent of 75e08ca (refactored gravity but it doesn't work)
+            particle.force = np.zeros(N_DIM)
             for other_particle in self.particles[self.particles != particle]:
-                distance = other_particle.position - particle.position
-                partial_acceleration = other_particle.mass / (
-                        np.linalg.norm(distance) ** 2
-                )
-                partial_acceleration *= unit(distance)
-                particle.acceleration += partial_acceleration
+                particle.resulting_force += Universe.tick_two_by_two(particle, other_particle)
 
-<<<<<<< HEAD
             particle.resulting_force *= self.constant
             particle.compute_state(dtime)
-
-    def __iter__(self):
-        return iter(self.particles)
-=======
-            particle.acceleration *= self.constant
-            particle.velocity = particle.acceleration * dtime
-            particle.position += particle.velocity * dtime
->>>>>>> parent of 75e08ca (refactored gravity but it doesn't work)
